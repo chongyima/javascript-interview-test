6b7cb51787616ca62204499b80e63e3776db0664
chongyima <chongyima2011@gmail.com>
2022-04-16 14:57:31 -0400

    updated string convert/index.js

--- string convert/index.js
+++ string convert/index.js
@@ -1,3 +1,4 @@
 // minusminusplusplus -> "--++"
 const str = 'minusminusplusplus';
 console.log(str.replace(new RegExp('minus', 'g'), '-').replace(new RegExp('plus', 'g'), '+'));
+console.log(str.replaceAll(/minus/g, '-').replaceAll(/plus/g, '+'));
